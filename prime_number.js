14c404d39a0eba74118691f0fcae983f3318237c
sambhaji dhore <sambhajisd4@gmail.com>
2022-11-03 15:37:54 +0530

    prime code updated

--- prime_number.js
+++ prime_number.js
@@ -1,6 +1,6 @@
 function PrimeNumber(Number){
     let Factors=0;
-    for(i=2;i<=Number/2;i++){
+    for(i=2;i<=Number*(1/2);i++){
         if(Number%i==0){
             Factors++;
             break;
@@ -15,7 +15,7 @@ function PrimeNumber(Number){
 let userInput=3274
 let answer=PrimeNumber(userInput);
 if(answer==true){
-    console.log("Prime number");
+    console.log(Number,"is Prime number");
 }else{
-    console.log("Not a prime number");
+    console.log(Number,"is Not a prime number");
 }
